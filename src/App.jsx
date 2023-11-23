0ff4442969dd2470cfac2f5233559d430916b3a9
Rubby <rubenslombard@pursuit.org>
2023-11-23 08:14:20 -0500

    modified app.jsx and page/show

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import New from "./Pages/New";
 import Edit from "./Pages/Edit";
 import Index from "./Pages/Index";
 import Home from "./Pages/Home";
-import Banksdata from "./Components/Banksdata";
+// import Banksdata from "./Components/Banksdata";
 
 function App() {
  
@@ -19,8 +19,8 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/banksdata" element={<Banksdata />} />
-            {/* <Route path="/new" element={<New />} /> */}
+            <Route path="/banksdata" element={<Index />} />
+            <Route path="/new" element={<New />} />
             {/* <Route exact path="/:id" element={<Show />} /> */}
             {/* <Route path="/:id/edit" element={<Edit />} /> */}
             {/* <Route path="*" element={<FourOFour />} /> */}
--- src/Pages/Show.jsx
+++ src/Pages/Show.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Banksdata from "../Components/Banksdata";
+import Bankdata from "../Components/Banksdata";
 
 function Show() {
   return (
     <div className="Show">
       <h2>Show</h2>
-      <Banksdata />
+      <Bankdata />
     </div>
   );
 }
